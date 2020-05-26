ff0c79eb4b9f73a70972ca16ceb1ca40979e409e
charles-a-beswick <65359396+charles-a-beswick@users.noreply.github.com>
2020-05-26 16:36:12 +0100

    Update RockPaperScissorsLizardSpock.cpp

--- RockPaperScissorsLizardSpock.cpp
+++ RockPaperScissorsLizardSpock.cpp
@@ -1,7 +1,9 @@
 /*
+Created as part of a Codecademy course.
+
 	Prompts the user to select either Rock, Paper, Scissors, Lizard, or Spock.
 	Instructs the computer to randomly select either Rock, Paper, Scissors, Lizard, or Spock.
-	Compares the user’s choice and the computer’s choice and determine the winner.
+	Compares the userâ€™s choice and the computerâ€™s choice and determine the winner.
 	Informs the user who the winner is.
 
 Logic:
